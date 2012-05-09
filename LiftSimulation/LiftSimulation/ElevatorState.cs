79667b11d05629baf9dd9f06910b0fdaf5d2c664
Paul Kunze <pkunze@hotmail.de>
2012-05-09 20:06:14 +0200

    erstmal n bierli trinken

---
+++ LiftSimulation/LiftSimulation/ElevatorState.cs
@@ -0,0 +1,34 @@
+﻿#region using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace LiftSimulation
+{
+    /// <summary>
+    /// State-Class, nur Methoden, keine Member!
+    /// </summary>
+    class ElevatorState
+    {
+    }
+
+    /// <summary>
+    /// State-Class, nur Methoden, keine Member!
+    /// </summary>
+    class FixedOpen : ElevatorState
+    {
+    }
+
+    /// <summary>
+    /// State-Class, nur Methoden, keine Member!
+    /// </summary>
+    class FixedClosed : ElevatorState
+    {
+    }
+
+    class Moving : ElevatorState 
+    {
+    }
+}
